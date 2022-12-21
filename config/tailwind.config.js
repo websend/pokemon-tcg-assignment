ac3249965a878ad901376b8dcbf6d07618a3ebcd
DeRidder <p.b.deridder@hotmail.com>
2022-12-21 22:47:44 +0100

    add basic styling

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,7 +14,11 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
   plugins: [
+    require('daisyui'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
